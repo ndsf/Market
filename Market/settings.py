ceed519484397542a6d8c2b82f4c560c279587cd
ndsf <ndsf@qq.com>
2019-05-20 16:08:54 +0800

    add requirements

--- Market/settings.py
+++ Market/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.sitemaps',
-    'django.contrib.postgres',
     'taggit',
     'article.apps.ArticleConfig',
     'information.apps.InformationConfig',
